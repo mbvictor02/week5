71c9e143350f6eed40fb0a0d5bac91849de817da
mbvictor <85256180+mbvictor02@users.noreply.github.com>
2023-10-28 14:13:35 +0200

    BankAccount.py

--- bankAccount.py
+++ bankAccount.py
@@ -1,32 +1,51 @@
 class BankAcc:
-    def __init__(self, account_number, balance = 0):
+    # Base class for a simple bank account
+    def __init__(self, account_number, balance=0):
+        # Initialize the account with an account number and balance
         self.account_number = account_number
         self.balance = balance
-    
-    def get_account_number(self):
-        return self.account_number
-    
-    def get_balance(self):
-        return self.balance
-    
-    def deposit(self, deposit):
-        self.balance += deposit
-    
-    def withdraw(self, withdraw):
-        self.balance -= withdraw
-    
-    #def __str__(self):
+
+    def get_account_details(self):
+        # Retrieve account details including account number and balance
+        return f"Account Number: {self.account_number}, Balance: {self.balance}"
+
+    def deposit(self, amount):
+        # Deposit funds into the account
+        self.balance += amount
+
+    def withdraw(self, amount):
+        # Withdraw funds from the account if sufficient balance exists
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("Insufficient funds.")
 
 class SavingsAccount(BankAcc):
-    def __init__(interest_rate,interests_generated):
-        super().__init__(interest_rate,interests_generated)
+    # Subclass inheriting from BankAcc for a savings account
+    def __init__(self, account_number, balance=0, interest_rate=0):
+        # Initialize the savings account with an interest rate
+        super().__init__(account_number, balance)
+        self.interest_rate = interest_rate
+
+    def generate_interest(self):
+        # Calculate and add interest to the account based on the balance and interest rate
+        interest = self.balance * self.interest_rate
+        self.balance += interest
+
+    def change_interest_rate(self, new_rate):
+        # Change the interest rate for the savings account
+        self.interest_rate = new_rate
 
 class CheckingAccount(BankAcc):
-    def __init__(overdraft_limit):
-        super().__init__(overdraft_limit)
+    # Subclass inheriting from BankAcc for a checking account
+    def __init__(self, account_number, balance=0, transaction_fee=0):
+        # Initialize the checking account with a transaction fee
+        super().__init__(account_number, balance)
+        self.transaction_fee = transaction_fee
 
-bank_account1 = BankAcc("ES9898989898", 10)
-bank_account1.deposit(5)
-print(f"The account {bank_account1.get_account_number()} has {bank_account1.get_balance()}") 
-
-#IMPORTANTE = entrara definicion del OPP
+    def withdraw(self, amount):
+        # Withdraw funds, considering a transaction fee
+        if self.balance >= amount + self.transaction_fee:
+            self.balance -= amount + self.transaction_fee
+        else:
+            print("Insufficient funds.")
